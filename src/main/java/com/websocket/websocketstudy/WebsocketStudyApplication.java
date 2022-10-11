8147917d321be7fa1713873bd9839ec322c3de5e
ohoraming <kminsmile@gmail.com>
2022-10-12 01:42:51 +0900

    초기 설정

---
+++ src/main/java/com/websocket/websocketstudy/WebsocketStudyApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.websocketstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/websocket/websocketstudy/WebsocketStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.websocketstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
